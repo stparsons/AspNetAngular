b1781bbde86ff34d935752154991738cc66b30a6
Stephen Parsons <zup_dudes@yahoo.com>
2018-10-12 14:49:20 -0400

    Build SPA application with Angular - Update App.Module and remove unit test files for now

--- SugarLevelTracker/Angular/src/app/app.module.ts
+++ SugarLevelTracker/Angular/src/app/app.module.ts
@@ -1,20 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatListModule,
+  MatToolbarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
 
 import { AppComponent } from './app.component';
-import { SugarlevelListComponent } from './sugarlevel-list/sugarlevel-list.component';
-import { SugarlevelEditComponent } from './sugarlevel-edit/sugarlevel-edit.component';
+import { SugarLevelListComponent } from './sugarlevel-list/sugarlevel-list.component';
+import { SugarLevelEditComponent } from './sugarlevel-edit/sugarlevel-edit.component';
+import SugarLevelService from './shared/api/sugar-level.service';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/sugarlevel-list', pathMatch: 'full' },
+  {
+    path: 'sugarlevel-list',
+    component: SugarLevelListComponent
+  },
+  {
+    path: 'sugarlevel-add',
+    component: SugarLevelEditComponent
+  },
+  {
+    path: 'sugarlevel-edit/:id',
+    component: SugarLevelEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SugarlevelListComponent,
-    SugarlevelEditComponent
+    SugarLevelListComponent,
+    SugarLevelEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
   ],
-  providers: [],
+  providers: [SugarLevelService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
