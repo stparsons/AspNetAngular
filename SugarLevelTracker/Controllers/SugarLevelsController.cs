200823eebc1c08f1473f383bc8a22daae7d90ae6
Stephen Parsons <zup_dudes@yahoo.com>
2018-10-12 15:23:02 -0400

    Protect ASP.NET API Endpoints

--- SugarLevelTracker/Controllers/SugarLevelsController.cs
+++ SugarLevelTracker/Controllers/SugarLevelsController.cs
@@ -14,6 +14,7 @@ using SugarLevelTracker.Models;
 
 namespace SugarLevelTracker.Controllers
 {
+    [Authorize]
     public class SugarLevelsController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
