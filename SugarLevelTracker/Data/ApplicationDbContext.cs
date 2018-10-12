442683adfcfcc6266291596d42171b492a289fff
Stephen Parsons <zup_dudes@yahoo.com>
2018-10-12 13:15:41 -0400

    Create an ASP.NET Model and DbContext

---
+++ SugarLevelTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using SugarLevelTracker.Models;
+
+namespace SugarLevelTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+          base( "OktaConnectionString" )
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<SugarLevel> SugarLevels { get; set; }
+    }
+}
